bc4b55f0c55ba78ea1d009ca82aa255d02949775
onynovianti <95726641+onynovianti@users.noreply.github.com>
2022-02-28 14:53:59 +0700

    Add files via upload

---
+++ storage/framework/views/14a85363bbb17c6f42eebe518172a330b59ccca5.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>BioData - Home</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" type="image/x-icon" href="img/favicon.ico">
+	<script type="text/javascript" src="js/js.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<!-- NAVBAR -->
+	<div class="topnav">
+	  <h2 id="logo"><b>BioData.</b></h2>
+	  <a href="#sosial_media">Sosial Media</a>
+	  <a href="#card3">Keahlian</a>
+	  <a href="#card2">Pendidikan</a>
+	  <a href="#card1">Tentang Saya</a>
+	  <a href="#home" class="aktif">Home</a>
+	</div>
+
+	<div class="header" id="home">
+		<img src="img/saya.jpg" width="220" height="220" style="border-radius: 50%; float: left;">
+	  <h1 style="text-align: left; margin-left: 30%;"><!-- <b>HALLO<b><br> -->ONY NOVIANTI</h1>
+	  	<hr>
+	  <h2 style="margin-left: 30%;">Mahasiswi</h2>
+	</div>
+
+	<button type="button" class="cta"></button>
+
+	<div class="row">
+	  <div class="leftcolumn">
+	    <div class="card" id="card1">
+	      <h2 style="border-left: solid black 2px;">&nbsp;TENTANG SAYA</h2>
+
+	      <p>Saya adalah mahasiswi Politeknik Negeri Malang, Jurusan Teknologi Informasi, Prodi D-IV Teknik Informatika. Saya belajar mengenai Pemrograman, Jaringan Komputer, Basis Data, dan banyak lainnya.</p>
+	      
+	      <br>
+	      <h4><img src="img/icons/name.png" width="15" height="15">&nbsp; Nama Lengkap</h4>
+	      <p style="margin-left: 24px;">Ony Novianti</p>
+	      <h4><img src="img/icons/cake.png" width="15" height="15">&nbsp; Tempat, Tanggal Lahir</h4>
+	      <p style="margin-left: 24px;">Malang, 26 November 2001</p>
+	      <h4><img src="img/icons/home-address.png" width="15" height="15">&nbsp; Alamat</h4>
+	      <p style="margin-left: 24px;">Sumberpucung, Kabupaten Malang</p>
+	      <h4><img src="img/icons/email-address.png" width="15" height="15">&nbsp; Email</h4>
+	      <p style="margin-left: 24px;">onynovianti26@gmail.com</p>
+	    
+	    </div>
+	    <div class="card" id="card2">
+	      <h2 style="border-left: solid black 2px;">&nbsp;PENDIDIKAN</h2>
+	      <ul>
+	      	<li>2008-2014 SDN Sumberpucung 07</li>
+	      	<li>2014-2017 SMPN 2 Sumberpucung</li>
+	      	<li>2017-2020 SMK Brantas Karangkates</li>
+	      </ul>
+	    </div>
+	    <div class="card" id="card3">
+	      <h2 style="border-left: solid black 2px;">&nbsp;KEAHLIAN</h2>
+	      <div class="card_logo">
+	      	<img src="img/icons/html.png" width="70" height="70">
+	      </div>
+	      <div class="card_logo">
+	      	<img src="img/icons/css.png" width="70" height="70">
+	      </div>
+	      <div class="card_logo">
+	      	<img src="img/icons/javascript.png" width="70" height="70">
+	      </div>
+	      <div class="card_logo">
+	      	<img src="img/icons/java.png" width="70" height="70">
+	      </div>
+	      <div class="card_logo">
+	      	<img src="img/icons/php.png" width="70" height="70">
+	      </div>
+	      <div class="card_logo">
+	      	<img src="img/icons/mysql.png" width="70" height="70">
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="footer" id="sosial_media">
+	  <h2>KONTAK</h2><hr>
+	  <div class="blok">
+	  	  <img src="img/icons/phone-call.png" width="30" height="30">
+		  <h3>Nomor HP</h3>
+		  <p>088217762015</p>
+		</div>
+		<div class="blok">
+			<img src="img/icons/facebook.png" width="30" height="30">
+		  <h3>Facebook</h3>
+		  <p>ony novianti</p>
+		</div>
+		<div class="blok">
+			<img src="img/icons/instagram.png" width="30" height="30">
+		  <h3>Instagram</h3>
+		  <p>onynovianti</p>
+	  </div>
+	</div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\laravel\resources\views/biodata.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e92d2b844ea86e3086251290e0751c86452595b8.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Lock - Welcoming Page</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" type="image/x-icon" href="img/favicon.ico">
+	<script type="text/javascript" src="js/js.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+</head>
+<body>
+	<!-- NAVBAR -->
+	<div class="topnav">
+	  <a href="#" id="logo"><b>BioData</b></a>
+	  <a href="#">Home</a>
+	  <a href="#">Pendidikan</a>
+	  <a href="#">Keahlian</a>
+	  <a href="#">Sosial Media</a>
+	  <a href="#" style="float:right" id="talk">Mari Berbincang!</a>
+	</div>
+
+	<!-- HEADER -->
+
+	<img src="img/hero.png" style="float: right;">
+	<div class="header">
+	  <h1>Hello!<span class="birudongker"> Saya Ony Novianti</span></h1>
+	  <p>Saat ini saya adalah mahasiswi Politeknik Negeri Malang, Jurusan Teknologi Informasi, Prodi D-IV Teknik Informatika, Kelas 2A.</p>
+	</div>
+
+
+	<div class="row">
+	  <div class="leftcolumn">
+	    <div class="card">
+	      <h2>TITLE HEADING</h2>
+	      <h5>Title description, Dec 7, 2017</h5>
+	      <div class="fakeimg" style="height:200px;">Image</div>
+	      <p>Some text..</p>
+	      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+	    </div>
+	    <div class="card">
+	      <h2>TITLE HEADING</h2>
+	      <h5>Title description, Sep 2, 2017</h5>
+	      <div class="fakeimg" style="height:200px;">Image</div>
+	      <p>Some text..</p>
+	      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+	    </div>
+	  </div>
+	  <div class="rightcolumn">
+	    <div class="card">
+	      <h2>About Me</h2>
+	      <div class="fakeimg" style="height:100px;">Image</div>
+	      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+	    </div>
+	    <div class="card">
+	      <h3>Popular Post</h3>
+	      <div class="fakeimg"><p>Image</p></div>
+	      <div class="fakeimg"><p>Image</p></div>
+	      <div class="fakeimg"><p>Image</p></div>
+	    </div>
+	    <div class="card">
+	      <h3>Follow Me</h3>
+	      <p>Some text..</p>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="footer">
+	  <h2>Footer</h2>
+	</div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\laravel\resources\views/lock.blade.php ENDPATH**/ ?>
