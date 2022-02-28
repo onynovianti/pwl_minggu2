244c83e8150de2ef5142e4b1e4712f7cbe2976a8
onynovianti <95726641+onynovianti@users.noreply.github.com>
2022-02-28 15:06:53 +0700

    Add files via upload

---
+++ resource/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>BioData - Home</title>
+		<link rel="stylesheet" href="css/ony.css">
+		<link rel="icon" type="image/x-icon" href="img/icons/logo.png">
+		<!-- <script type="text/javascript" src="js/js.js"></script> -->
+		<link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
+	</head>
+	<body class="body-home">
+		<!-- NAVBAR -->
+		<div class="home">
+			<h1 style="text-align: left; margin-left: 60%; margin-top: 10px; font-family: 'Crete Round', serif;">BioData.</h1>
+			<h3 style="margin-left: 60%; font-weight: 300;">Adalah website untuk membuat biodata Anda sendiri. Anda dapat menambahkan bagian-bagian apapun yang mencerminkan suatu hal di hidup Anda</h3>
+			<br>
+			<a style="margin-left:60%;" class="button-home" href="about">Lihat Contoh</a>
+		</div>
+	</body>
+</html>
